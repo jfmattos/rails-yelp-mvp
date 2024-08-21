24509b66eedc69e155377abbec37d9686f3e70f7
jasmim mattos <jasmimfm@gmail.com>
2024-08-21 12:26:26 -0300

    seeding the restaurant model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+mrchang = {name: "MR. Chang", address: "china street", phone_number: "12398722", category: "chinese"}
+
+pizzaplanet = {name: "Pizza Planet", address: "rome street", phone_number: "1222222", category: "italian"}
+
+jappa = {name: "Jappa", address: "tokyo street", phone_number: "3333333", category: "japanese"}
+
+ouioui = {name: "Oui Oui", address: "paris street", phone_number: "4444444", category: "french"}
+
+houseoffries = {name: "House of Fries", address: "antwerp street", phone_number: "5555555", category: "belgian"}
+
+[mrchang, pizzaplanet, jappa, ouioui, houseoffries].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
